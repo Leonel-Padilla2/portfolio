63b8968f31012aa6ca9184187b63333565bbdec5
Leonel-Padilla2 <axcelpadilla17@gmail.com>
2022-09-05 19:10:13 -0600

    feat: adding navbar at the main view

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,8 +1,14 @@
+import NavBar from '../components/NavBar/NavBar'
+import './App.css'
 
 const App = () => {
   return (
-    <div>
-      Hello
+    <div className="app-container">
+      <div className="content-container">
+        <NavBar/>
+
+                
+      </div>
     </div>
   );
 }
