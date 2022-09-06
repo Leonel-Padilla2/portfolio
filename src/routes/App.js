7da0442a6ce3fc60205694c4c4c4b992d21c811f
Leonel-Padilla2 <axcelpadilla17@gmail.com>
2022-09-06 13:15:46 -0600

    feature: adding routes structure

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,4 +1,5 @@
 import NavBar from '../components/NavBar/NavBar'
+import { Routes, Route } from 'react-router-dom';
 import './App.css'
 
 const App = () => {
@@ -6,8 +7,12 @@ const App = () => {
     <div className="app-container">
       <div className="content-container">
         <NavBar/>
-
-                
+          
+        <Routes>
+          <Route path='/' element={<h1>Home Page</h1>}/>
+          <Route path='/projects' element={<h1>Projects Page</h1>}/>
+          <Route path='/contact' element={<h1>Get in touch Page</h1>}/>
+        </Routes>
       </div>
     </div>
   );
