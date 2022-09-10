d9fabfdbb8463072f0b5e63002b84d8f5e014a19
Leonel-Padilla2 <axcelpadilla17@gmail.com>
2022-09-10 15:04:33 -0600

    refactor: editing navbar template in order to attach it at the project

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,24 +1,51 @@
 import React from 'react'
 import { useState } from 'react'
 import menuLogo from  '../../images/menu-logo.png'
+import { Link } from 'react-router-dom'
 import './NavBar.css'
 
 const NavBar = () => {
   const [activated, setActivated] = useState(false)
+  
+  const toggleActive = () => {
+    setActivated(curreent => !curreent)
+  }
+
+
   return (
     <header className={`navbar-container ${activated ? 'active' : ''}`}>
 
       <h2>LOGO</h2>
 
-      <button onClick={()=> setActivated(curreent => !curreent)} className='navbar-displayer'>
+      <button 
+        className='navbar-displayer'
+        onClick={toggleActive} 
+      >
         <img src={menuLogo} alt="menu logo" />
       </button>
 
       <nav className='list-container'>
         <ul className='items-list'>
-          <li className='navbar-item'>Home</li>
-          <li className='navbar-item'>Projects</li>
-          <li className='navbar-item'>About Me</li>
+          <Link 
+            className='navbar-link'
+            to='/'
+          >
+            <li className='navbar-item'>Home</li>
+          </Link>
+
+          <Link 
+            className='navbar-link' 
+            to='/projects'
+          >
+            <li className='navbar-item'>Projects</li>
+          </Link>
+          
+          <Link 
+            className='navbar-link'
+            to='/contact'
+          >
+            <li className='navbar-item'>Contact Me</li>
+          </Link>
         </ul>
       </nav>
 
