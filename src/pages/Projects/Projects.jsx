da181f37f71e009bdf7f5a994c1a6331888e4013
Leonel-Padilla2 <axcelpadilla17@gmail.com>
2022-09-26 17:14:18 -0600

    feat: Porjects page mockup

---
+++ src/pages/Projects/Projects.jsx
@@ -0,0 +1,81 @@
+import './Projects.css'
+
+const Projects = () => {
+  return (
+    <div className='projects-container'>
+      <h2>These are the projects I have worked on</h2>        
+
+      <div className='projects-grid'>
+
+        <div className='card'>
+          <img src='' alt="project-illustation"/>
+          <h2>Project Title</h2>
+
+          <div className='card-overlay'>
+            Project URLs
+          </div>
+        </div>
+        <div className='card'>
+          <img src='' alt="project-illustation"/>
+          <h2>Project Title</h2>
+
+          <div className='card-overlay'>
+            Project URLs
+          </div>
+        </div>
+        <div className='card'>
+          <img src='' alt="project-illustation"/>
+          <h2>Project Title</h2>
+
+          <div className='card-overlay'>
+            Project URLs
+          </div>
+        </div>
+        <div className='card'>
+          <img src='' alt="project-illustation"/>
+          <h2>Project Title</h2>
+
+          <div className='card-overlay'>
+            Project URLs
+          </div>
+        </div>
+        <div className='card'>
+          <img src='' alt="project-illustation"/>
+          <h2>Project Title</h2>
+
+          <div className='card-overlay'>
+            Project URLs
+          </div>
+        </div>
+        <div className='card'>
+          <img src='' alt="project-illustation"/>
+          <h2>Project Title</h2>
+
+          <div className='card-overlay'>
+            Project URLs
+          </div>
+        </div>
+        <div className='card'>
+          <img src='' alt="project-illustation"/>
+          <h2>Project Title</h2>
+
+          <div className='card-overlay'>
+            Project URLs
+          </div>
+        </div>
+        <div className='card'>
+          <img src='' alt="project-illustation"/>
+          <h2>Project Title</h2>
+
+          <div className='card-overlay'>
+            Project URLs
+          </div>
+        </div>
+
+      </div>
+      
+    </div>
+  )
+}
+
+export default Projects
