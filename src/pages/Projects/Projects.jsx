e37d2c1c01c45d8f8b16ebc43f8c11e9261423f2
Leonel-Padilla2 <axcelpadilla17@gmail.com>
2022-09-27 23:49:05 -0600

    feat: adding media queries on projects grid

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -9,11 +9,51 @@ const Projects = () => {
       gitHubURL: '',
       deployedURL: '',
       sumary: 'This is my personal portfolio which I have developed in order to show my personal projects.'
+    },
+    {
+      img: '',
+      title : 'Personal Portfolio',
+      gitHubURL: '',
+      deployedURL: '',
+      sumary: 'This is my personal portfolio which I have developed in order to show my personal projects.'
+    },
+    {
+      img: '',
+      title : 'Personal Portfolio',
+      gitHubURL: '',
+      deployedURL: '',
+      sumary: 'This is my personal portfolio which I have developed in order to show my personal projects.'
+    },
+    {
+      img: '',
+      title : 'Personal Portfolio',
+      gitHubURL: '',
+      deployedURL: '',
+      sumary: 'This is my personal portfolio which I have developed in order to show my personal projects.'
+    },
+    {
+      img: '',
+      title : 'Personal Portfolio',
+      gitHubURL: '',
+      deployedURL: '',
+      sumary: 'This is my personal portfolio which I have developed in order to show my personal projects.'
+    },
+    {
+      img: '',
+      title : 'Personal Portfolio',
+      gitHubURL: '',
+      deployedURL: '',
+      sumary: 'This is my personal portfolio which I have developed in order to show my personal projects.'
     }
   ]
+
   return (
     <div className='projects-container'>
       <h2>These are the projects I have worked on</h2>        
+      <p>
+        All projects here helped me to improve my development skills, every single project is totaly different with each other, 
+        that's why all of them compelled me to investigate, understand and learn new skills and subjects necesary for development.
+      </p>
 
       <div className='projects-grid'>
 
