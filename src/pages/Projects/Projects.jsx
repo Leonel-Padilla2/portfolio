a7bfc9821969e0482f1680d6b7dcc1eb15116e5e
Leonel-Padilla2 <axcelpadilla17@gmail.com>
2022-10-04 10:23:42 -0600

    feat: adding real projects at the projects list

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,52 +1,34 @@
 import ProjectCard from '../../components/ProjectCard/ProjectCard'
 import PortFolioImage from '../../images/portfolio-image.png'
+import PomodoroImgae from '../../images/pomodoro-image.png'
 import './Projects.css'
 
+const projects = [
+  {
+    img: PortFolioImage,
+    title : 'Personal Portfolio',
+    gitHubURL: 'https://github.com/Leonel-Padilla2/portfolio',
+    deployedURL: '/',
+    sumary: 'This is my personal portfolio which I have developed in order to show my personal projects, also you can contact me and read a bit more about me.'
+  },
+  {
+    img: PomodoroImgae,
+    title : 'Pomodoro Application',
+    gitHubURL: 'https://github.com/Leonel-Padilla/fpomodoro',
+    deployedURL: '/',
+    sumary: 'This application were developed as part of an event, this project was made with a team from people all over America, using Agile Methodologies.'
+  },
+  {
+    img: '',
+    title : 'Restaurant Application',
+    gitHubURL: '/',
+    deployedURL: '/',
+    sumary: 'This web application was developed as a class project which name was "Software Engineering" this is a restaurant application for inventory, clients, employees, purchasing and billing management.'
+  }
+]
+
 const Projects = () => {
-  const projects = [
-    {
-      img: PortFolioImage,
-      title : 'Personal Portfolio',
-      gitHubURL: 'https://github.com/Leonel-Padilla2/portfolio',
-      deployedURL: '',
-      sumary: 'This is my personal portfolio which I have developed in order to show my personal projects.'
-    },
-    {
-      img: '',
-      title : 'Personal Portfolio',
-      gitHubURL: '',
-      deployedURL: '',
-      sumary: 'This is my personal portfolio which I have developed in order to show my personal projects.'
-    },
-    {
-      img: '',
-      title : 'Personal Portfolio',
-      gitHubURL: '',
-      deployedURL: '',
-      sumary: 'This is my personal portfolio which I have developed in order to show my personal projects.'
-    },
-    {
-      img: '',
-      title : 'Personal Portfolio',
-      gitHubURL: '',
-      deployedURL: '',
-      sumary: 'This is my personal portfolio which I have developed in order to show my personal projects.'
-    },
-    {
-      img: '',
-      title : 'Personal Portfolio',
-      gitHubURL: '',
-      deployedURL: '',
-      sumary: 'This is my personal portfolio which I have developed in order to show my personal projects.'
-    },
-    {
-      img: '',
-      title : 'Personal Portfolio',
-      gitHubURL: '',
-      deployedURL: '',
-      sumary: 'This is my personal portfolio which I have developed in order to show my personal projects.'
-    }
-  ]
+
 
   return (
     <div className='projects-container'>
