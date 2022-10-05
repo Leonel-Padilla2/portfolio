02fba8c4554e80b77e5807066d6620f8661eae1c
Leonel-Padilla2 <axcelpadilla17@gmail.com>
2022-10-04 21:21:46 -0600

    feat: adding new project at the projects list

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,34 +1,34 @@
 import ProjectCard from '../../components/ProjectCard/ProjectCard'
 import PortFolioImage from '../../images/portfolio-image.png'
-import PomodoroImgae from '../../images/pomodoro-image.png'
+import PomodoroImage from '../../images/pomodoro-image.png'
+import RestaurantImage from '../../images/New-Project.jpg'
 import './Projects.css'
 
 const projects = [
+  {
+    img: RestaurantImage,
+    title : 'Restaurant Application',
+    gitHubURL: '/',
+    deployedURL: '/',
+    sumary: 'This web application was developed as a class project which name was "Software Engineering" this is a restaurant application for inventory, clients, employees, purchasing and billing management.'
+  },
+  {
+    img: PomodoroImage,
+    title : 'Pomodoro Application',
+    gitHubURL: 'https://github.com/Leonel-Padilla/fpomodoro',
+    deployedURL: '/',
+    sumary: 'This application was developed as part of an event, this project was made with a team from people all over America, using Agile Methodologies.'
+  },
   {
     img: PortFolioImage,
     title : 'Personal Portfolio',
     gitHubURL: 'https://github.com/Leonel-Padilla2/portfolio',
     deployedURL: '/',
-    sumary: 'This is my personal portfolio which I have developed in order to show my personal projects, also you can contact me and read a bit more about me.'
-  },
-  {
-    img: PomodoroImgae,
-    title : 'Pomodoro Application',
-    gitHubURL: 'https://github.com/Leonel-Padilla/fpomodoro',
-    deployedURL: '/',
-    sumary: 'This application were developed as part of an event, this project was made with a team from people all over America, using Agile Methodologies.'
-  },
-  {
-    img: '',
-    title : 'Restaurant Application',
-    gitHubURL: '/',
-    deployedURL: '/',
-    sumary: 'This web application was developed as a class project which name was "Software Engineering" this is a restaurant application for inventory, clients, employees, purchasing and billing management.'
+    sumary: 'This is my personal portfolio which I have developed in order to show my personal projects, you can contact me and read a bit more about me as well.'
   }
 ]
 
 const Projects = () => {
-
 
   return (
     <div className='projects-container'>
@@ -48,6 +48,7 @@ const Projects = () => {
       
     </div>
   )
+
 }
 
 export default Projects
