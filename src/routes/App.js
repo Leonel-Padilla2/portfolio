b6bfd77e9d1d3d403f73589ac4f874c56e2021bf
Leonel-Padilla2 <axcelpadilla17@gmail.com>
2022-10-08 22:32:59 -0600

    feat: using Modal Context Provider to wrap the whole application and using useContext hook to get ModalContext value on App.js

--- src/routes/App.js
+++ src/routes/App.js
@@ -4,10 +4,24 @@ import Home from '../pages/Home/Home'
 import Projects from '../pages/Projects/Projects';
 import Contact from '../pages/Contact/Contact'
 import './App.css'
+import { useContext } from 'react';
+import { ModalContext } from '../contexts/ModalContext';
+import Modal from '../components/Modal/Modal'
 
 const App = () => {
+  const {modalData, onClose} = useContext(ModalContext)
+  const {visible, title, message} = modalData
+
   return (
     <div className="app-container">
+      <Modal 
+        title={title}
+        visible={visible}
+        onClose={onClose}
+      >
+        {message}
+      </Modal>
+
       <div className="content-container">
         <NavBar/>
           
